165e0040c53bfa0ad44d83625130f2445b1a4e76
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-10-30 15:17:36 -0400

    Update BoxServoTest.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoxServoTest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoxServoTest.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp
 public class BoxServoTest extends OpMode {
     CRServo testServo;
 
