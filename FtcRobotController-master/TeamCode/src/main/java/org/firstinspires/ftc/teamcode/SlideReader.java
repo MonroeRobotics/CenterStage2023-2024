f31aeecadc355b06ef1ba5366d6b912fd1aefd51
RobotNatchos <robot6895games@gmail.com>
2023-11-06 14:05:11 -0500

    Added Arm Test

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideReader.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class SlideReader extends OpMode {
+
+
+    DcMotorEx rightLinear;
+    DcMotorEx leftLinear;
+
+    //endregion
+
+
+    @Override
+    public void init() {
+        //region Hardware Map
+
+
+        leftLinear = hardwareMap.get(DcMotorEx.class ,"leftLinear");
+        rightLinear = hardwareMap.get(DcMotorEx.class, "rightLinear");
+
+        //endregion
+
+        //region Motor Settings
+        leftLinear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightLinear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftLinear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rightLinear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        //endregion
+    }
+    @Override
+    public void loop() {
+        telemetry.addData("Right Slide Height", rightLinear.getCurrentPosition());
+        telemetry.addData("left Slide Height", leftLinear.getCurrentPosition());
+        telemetry.update();
+    }
+
+
+
+    @Override
+    public void stop() {
+    }
+}
