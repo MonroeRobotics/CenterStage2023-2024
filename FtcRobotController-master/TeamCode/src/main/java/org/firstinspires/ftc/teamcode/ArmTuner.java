3e1dca9e80123f309325f3cd681a1e2a73a0fa1f
RobotNatchos <robot6895games@gmail.com>
2023-11-06 17:10:06 -0500

    Update ArmTuner.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTuner.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTuner.java
@@ -15,13 +15,14 @@ public class ArmTuner extends OpMode {
     public static int SLIDE_HEIGHT = 20;
     public static double SLIDE_POWER = 0.5;
 
-    public static double ARM_SERVO_POSITION = 0;
-    public static double ARM_SERVO_FORWARD_MAX = 0.1;
-    public static double ARM_SERVO_BACKWARD_MAX = -0.1;
+    public static double ARM_POSITION = 0.5;
+    public static double ARM_SERVO_FORWARD = 0.67;
+    public static double ARM_SERVO_BACKWARD = 0.15;
+    public static double LEFT_ARM_SERVO_OFFSET = -0.04;
 
-    public static double BOX_SERVO_POSITION = 0;
-    public static double BOX_SERVO_FORWARD_MAX = 0.1;
-    public static double BOX_SERVO_BACKWARD_MAX = -0.1;
+    public static double BOX_SERVO_POSITION = 0.5;
+    public static double BOX_SERVO_FORWARD = 0.83;
+    public static double BOX_SERVO_BACKWARD = 0.22;
 
     //region Declare Objects
     Servo armServoRight;
@@ -60,39 +61,36 @@ public class ArmTuner extends OpMode {
         //endregion
 
         //region Initial Servo Pos
-        armServoLeft.setPosition(ARM_SERVO_POSITION);
-        armServoRight.setPosition(ARM_SERVO_POSITION);
+        armServoLeft.setPosition(ARM_POSITION);
+        armServoRight.setPosition(ARM_POSITION);
         boxServo.setPosition(BOX_SERVO_POSITION);
         //endregion
     }
     @Override
     public void loop() {
-        if(gamepad1.a){
-
-            ARM_SERVO_POSITION = ARM_SERVO_FORWARD_MAX;
-        }
-        else if (gamepad1.b) {
-            ARM_SERVO_POSITION = ARM_SERVO_BACKWARD_MAX;
-        }
-
         if (gamepad1.x) {
-            BOX_SERVO_POSITION = BOX_SERVO_FORWARD_MAX;
+            BOX_SERVO_POSITION = BOX_SERVO_FORWARD;
+            ARM_POSITION = ARM_SERVO_FORWARD;
         }
         else if (gamepad1.y){
-            BOX_SERVO_POSITION = BOX_SERVO_BACKWARD_MAX;
+            BOX_SERVO_POSITION = BOX_SERVO_BACKWARD;
+            ARM_POSITION = ARM_SERVO_BACKWARD;
         }
 
         leftLinear.setTargetPosition(SLIDE_HEIGHT);
         rightLinear.setTargetPosition(SLIDE_HEIGHT);
 
-        armServoLeft.setPosition(ARM_SERVO_POSITION);
-        armServoRight.setPosition(ARM_SERVO_POSITION);
+        armServoLeft.setPosition(ARM_POSITION);
+        armServoRight.setPosition(1 - ARM_POSITION + LEFT_ARM_SERVO_OFFSET);
 
         boxServo.setPosition(BOX_SERVO_POSITION);
 
         telemetry.addData("Slide Target Height", SLIDE_HEIGHT);
         telemetry.addData("Right Slide Height", rightLinear.getCurrentPosition());
         telemetry.addData("left Slide Height", leftLinear.getCurrentPosition());
+        telemetry.addData("Right Servo Pos:", armServoRight.getPosition());
+        telemetry.addData("Left Servo Pos:", armServoLeft.getPosition());
+
         telemetry.update();
     }
 
