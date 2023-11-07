60f09099467b3285e69bc38ae9b58f84c0d2c278
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2023-11-07 17:27:04 -0500

    Added bottom right trajectories to spike marks.

--- FtcRobotController-master/RoadRunnerSim/src/main/java/com/example/roadrunnersim/RoadRunnerSim.java
+++ FtcRobotController-master/RoadRunnerSim/src/main/java/com/example/roadrunnersim/RoadRunnerSim.java
@@ -1,6 +1,7 @@
 package com.example.roadrunnersim;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
@@ -20,29 +21,44 @@ public class RoadRunnerSim {
         MeepMeep meepMeep = new MeepMeep(800);
 
         // Declare our first bot
-        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity firstBot = new DefaultBotBuilder(meepMeep)
+                .setDimensions(14,16)
                 // We set this bot to be blue
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(10, -62, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(32.75,-30, Math.toRadians(180)))
                                 .build()
                 );
 
-
+        RoadRunnerBotEntity secondBot = new DefaultBotBuilder(meepMeep)
+                .setDimensions(14,16)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(10, -62, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(20,-24, Math.toRadians(180)))
+                                .build()
+                );
+        RoadRunnerBotEntity thirdBot = new DefaultBotBuilder(meepMeep)
+                .setDimensions(14,16)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(10, -62, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(10,-30, Math.toRadians(180)))
+                                .build()
+                );
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 // Add both of our declared bot entities
-                .addEntity(myFirstBot)
+                .addEntity(firstBot)
+                .addEntity(secondBot)
+                .addEntity(thirdBot)
                 .start();
     }
 }
