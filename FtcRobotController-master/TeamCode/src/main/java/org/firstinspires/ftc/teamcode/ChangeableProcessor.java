0db86cc78e395ef5fa3858614785dfa66c953fde
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2023-11-07 16:04:25 -0500

    A can switch between red detection and april tage detection

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChangeableProcessor.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.TeamPropDetection;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@Autonomous
+public class ChangeableProcessor extends OpMode {
+    TeamPropDetection propDetector;
+    AprilTagProcessor aprilTagDetector;
+    VisionPortal visionPortal;
+    String screenSector;
+
+    boolean cameraState = false;
+    boolean notPressed = true;
+    @Override
+    public void init() {
+        propDetector = new TeamPropDetection(telemetry);
+        aprilTagDetector = AprilTagProcessor.easyCreateWithDefaults();
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "webcam"), aprilTagDetector, propDetector);
+        visionPortal.setProcessorEnabled(aprilTagDetector, false);
+    }
+
+    @Override
+    public void loop() {
+        screenSector = propDetector.getScreenSector();
+        telemetry.addData("Screen sector", screenSector);
+        if (gamepad1.a  && notPressed){
+            notPressed = false;
+            cameraState = !cameraState;
+            if (cameraState == false){
+                visionPortal.setProcessorEnabled(aprilTagDetector, false);
+                visionPortal.setProcessorEnabled(propDetector, true);
+            }
+            else if (cameraState == true){
+                visionPortal.setProcessorEnabled(aprilTagDetector, true);
+                visionPortal.setProcessorEnabled(propDetector, false);
+            }
+        }
+        if (!gamepad1.a){
+            notPressed = true;
+        }
+    }
+}
