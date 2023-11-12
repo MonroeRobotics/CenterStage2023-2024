6c3e8206afa47addaf6acc90f3cdde46460e957a
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-11-12 16:03:04 -0500

    Update AkhilSpinnyThing.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AkhilSpinnyThing.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AkhilSpinnyThing.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
+@TeleOp
 public class AkhilSpinnyThing extends OpMode {
     DcMotor spinny;
     Servo uppy;
-    double motorPower = .5;
-    double servoPosition = .3;
+    public static double motorPower = .5;
+    public static double servoPosition = .5;
     boolean intakePosition = false;
     boolean pressed = false;
 //use dpad for intake and outtake
@@ -19,31 +23,31 @@ public class AkhilSpinnyThing extends OpMode {
         spinny = hardwareMap.get(DcMotor.class, "spinny");
         uppy = hardwareMap.get(Servo.class, "uppy");
         //setting motors runmode
-        spinny.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        spinny.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        uppy.setPosition(servoPosition);
     }
 
     @Override
     public void loop() {
         if (gamepad1.dpad_down) {
            spinny.setPower(motorPower);
+        } else if (gamepad1.dpad_up) {
+            spinny.setPower(-motorPower);
+        } else {
+            spinny.setPower(0);
         }
-        else {
-            //power zero
-        }
-        if (gamepad1.x){
-            if (pressed == false){
-                pressed = true;
-                intakePosition = !intakePosition;
-                if (intakePosition == true){
-                    uppy.setPosition(servoPosition);
-                }
-                else{
-                    uppy.setPosition(0);
-                }
-            }
 
+        if (gamepad1.x && pressed == false){
+            pressed = true;
+            intakePosition = !intakePosition;
+            if (intakePosition == true){
+                uppy.setPosition(0);
+            }
+            else{
+                uppy.setPosition(servoPosition);
+            }
         }
-        else{
+        else if (!gamepad1.x){
             pressed = false;
         }
     }
