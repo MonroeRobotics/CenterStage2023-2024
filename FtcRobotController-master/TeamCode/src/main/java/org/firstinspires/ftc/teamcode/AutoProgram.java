a67214c5342fb3d0ae933dbe3cd8017d30d8bd23
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2023-11-13 15:42:54 -0500

    Create AutoProgram.java

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgram.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Disabled
+@Autonomous(name = "Auto Program", group = "Main")
+public class AutoProgram extends OpMode {
+    SampleMecanumDrive drive;
+    
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
