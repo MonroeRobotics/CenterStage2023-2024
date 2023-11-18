29b97ef9591b23956fa9f30fe3aeeb12772e44d8
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2023-11-18 14:19:34 -0500

    Update AutoProgramRedBoard.java
    added vision stuffs to it so that yeah.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgramRedBoard.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgramRedBoard.java
@@ -12,46 +12,64 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.vision.AprilTagHomer;
+import org.firstinspires.ftc.vision.TeamPropDetection;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @Disabled
 @Autonomous(name = "Auto Program", group = "Main")
 public class AutoProgramRedBoard extends OpMode {
     SampleMecanumDrive drive;
-    ChangeableProcessor changeableProcessor = new ChangeableProcessor();
-    //trajectory declarations
+
+    //region trajectory declarations
     Trajectory redBoardCenter;
     Trajectory redBoardPlacement;
     Trajectory redBoardPark;
+    //endregion
 
     Trajectory universalTrajectory;
 
-    //slides
+    //region slides
     DcMotorEx leftSlide;
     DcMotorEx rightSlide;
     Servo leftArmServo;
     Servo rightArmServo;
+    //endregion
 
+    //region red board spike locations
     Pose2d spikeLocation;
     Pose2d spikeOne = new Pose2d(10,-30, Math.toRadians(180));
     Pose2d spikeTwo = new Pose2d(20,-24, Math.toRadians(180));
     Pose2d spikeThree = new Pose2d(13,-30, Math.toRadians(0));
+    //endregion
 
 
-    //intake and placement pieces
+    //region intake and placement pieces
     DcMotorEx intake;
     Servo uppy;
     Servo bucketServo;
     Servo bucketAngle;
+    //endregion
 
     double waitTime;
     double currentTime;
 
     WebcamName webcam;
-
     String screenSector;
     String colorDetectString = "";
     String colorDetected = "";
 
+    //region importing vision stuff
+    TeamPropDetection propDetection = new TeamPropDetection(telemetry);
+
+    AprilTagProcessor aprilTagDetector;
+    VisionPortal visionPortal;
+
+    AprilTagHomer aprilTagHomer;
+    //endregion
+
+    //region static coordinates
     Pose2d redBoardCord = new Pose2d(48, -35, Math.toRadians(180));
     Pose2d redParkCord = new Pose2d(48, -60, Math.toRadians(180));
     Pose2d blueBoardCord = new Pose2d(48, 35, Math.toRadians(180));
@@ -59,7 +77,7 @@ public class AutoProgramRedBoard extends OpMode {
 
     Pose2d redBoardCenterSpikeMark = new Pose2d(13, -30, Math.toRadians(0));
     Pose2d redBoardCenterSpikeMarkStart = new Pose2d(10,62, Math.toRadians(90));
-
+    //endregion
 
 
     enum State {
@@ -77,11 +95,15 @@ public class AutoProgramRedBoard extends OpMode {
 
     @Override
     public void init() {
+
+        //region intake parts hardwaremap
         intake = hardwareMap.get(DcMotorEx.class, "intake");
         uppy = hardwareMap.get(Servo.class, "uppy");
         bucketServo = hardwareMap.get(Servo.class, "bucketServo");
         bucketAngle = hardwareMap.get(Servo.class, "bucketAngle");
+        //endregion
 
+        //region slides stuff
         leftSlide = hardwareMap.get(DcMotorEx.class, "leftSlide");
         rightSlide = hardwareMap.get(DcMotorEx.class, "rightSlide");
 
@@ -105,6 +127,7 @@ public class AutoProgramRedBoard extends OpMode {
         leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightSlide.setPower(0.4);
         leftSlide.setPower(0.4);
+        //endregion
 
         //get webcam stuff here
 
@@ -113,6 +136,8 @@ public class AutoProgramRedBoard extends OpMode {
         drive.setPoseEstimate(new Pose2d(10, 62, Math.toRadians(90)));
 
         //Identify spike marker location
+
+        screenSector = propDetection.getScreenSector();
 
         if (screenSector == "L"){
             spikeLocation = spikeOne;
@@ -123,6 +148,13 @@ public class AutoProgramRedBoard extends OpMode {
         else{
             spikeLocation = spikeThree;
         }
+
+        aprilTagDetector = AprilTagProcessor.easyCreateWithDefaults();
+
+        aprilTagHomer = new AprilTagHomer(aprilTagDetector, drive);
+
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "webcam"), aprilTagDetector, propDetection);
+        visionPortal.setProcessorEnabled(aprilTagDetector, false);
 
         universalTrajectory = drive.trajectoryBuilder(drive.getPoseEstimate())
             .lineToLinearHeading(spikeLocation)
