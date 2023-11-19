7302aa57ea9e9b6f44a8b9fb810d408bf9165a6e
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-11-19 16:10:53 -0500

    Update ColorSensorColorFinder.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorColorFinder.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorColorFinder.java
@@ -2,21 +2,26 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
+@TeleOp
 public class ColorSensorColorFinder extends OpMode {
     RevColorSensorV3 colorSensor;
     int argbDetected;
 
     @Override
     public void init() {
-        colorSensor = (RevColorSensorV3) hardwareMap.get("colorSensor");
-    }
+        colorSensor = (RevColorSensorV3) hardwareMap.get("colorSensor1");}
 
     @Override
     public void loop() {
         argbDetected = colorSensor.argb();
         telemetry.addData("ARGB", argbDetected);
+        telemetry.addData("R", colorSensor.red());
+        telemetry.addData("G", colorSensor.green());
+        telemetry.addData("B", colorSensor.blue());
+
         telemetry.update();
     }
 }
