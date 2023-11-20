2c0badaee41ebf4044a898be20368a3ab3f5b24a
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-11-20 12:29:57 -0500

    Revert "Update AkhilSpinnyThing.java"
    This reverts commit 5fb81f15223c76f5a969b5938490752e374b8e75.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AkhilSpinnyThing.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AkhilSpinnyThing.java
@@ -1,51 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class AkhilSpinnyThing extends OpMode {
     DcMotor spinny;
-    Servo uppy;
     double motorPower = .5;
-    double servoPosition = .3;
-    boolean intakePosition = false;
-    boolean pressed = false;
-//use dpad for intake and outtake
-//1 motor for powering and one servo for up and down
+
     @Override
     public void init() {
         //mapping motors onto motor objects
         spinny = hardwareMap.get(DcMotor.class, "spinny");
-        uppy = hardwareMap.get(Servo.class, "uppy");
+
         //setting motors runmode
         spinny.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
-        if (gamepad1.dpad_down) {
-           spinny.setPower(motorPower);
-        }
-        else {
-            //power zero
-        }
-        if (gamepad1.x){
-            if (pressed == false){
-                pressed = true;
-                intakePosition = !intakePosition;
-                if (intakePosition == true){
-                    uppy.setPosition(servoPosition);
-                }
-                else{
-                    uppy.setPosition(0);
-                }
-            }
-
-        }
-        else{
-            pressed = false;
-        }
+        spinny.setPower(motorPower);
     }
 
 
