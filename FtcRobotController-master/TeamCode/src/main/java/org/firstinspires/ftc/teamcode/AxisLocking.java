0946df1ab2301e65afe69b2fad4fa3c352b3fca2
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-11-20 12:29:57 -0500

    Revert "Update AxisLocking.java"
    This reverts commit cf85776b890038bff16f63738ca9fc96c10280c4.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AxisLocking.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AxisLocking.java
@@ -5,9 +5,7 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp
@@ -15,34 +13,38 @@ public class AxisLocking extends OpMode {
 
     SampleMecanumDrive drive;
 
-    DistanceSensor leftDistanceSensor;
-    DistanceSensor rightDistanceSensor;
-
-    //this gotta be from 0<x<1
-    double alignHeadingFactor = .2;
     boolean axisLocked = false;
     boolean xNotPressed = true;
 
     @Override
     public void init() {
-        //intialize the things
+        //intialize the MecanumDrive thingy
         drive = new SampleMecanumDrive(hardwareMap);
-        leftDistanceSensor = hardwareMap.get(DistanceSensor.class, "leftDistanceSensor");
-        rightDistanceSensor = hardwareMap.get(DistanceSensor.class, "rightDistanceSensor");
 
         //turn off velocity control for reasons
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //put in intial pos and heading from auto mode (magic stuff)
+        drive.setPoseEstimate(new Pose2d(x, y, heading));
     }
 
     @Override
     public void loop() {
+        // Read pose
+        Pose2d poseEstimate = drive.getPoseEstimate();
 
-        //get them at base so I can mess with them if needed
-        double x = gamepad1.left_stick_x
-        double y = -gamepad1.left_stick_y;
-        double heading = gamepad1.right_stick_x;
+        // Create a vector from the gamepad x/y inputs
+        // Then, rotate that vector by the inverse of that heading
+        Vector2d input = new Vector2d(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x
+        ).rotated(-poseEstimate.getHeading());
 
-        //if gamepad1 x is clicked change axisLocked to other val
+        //get them out before being put into setWDP so I can mess with them
+        double x = input.getX();
+        double y = input.getY();
+
+        //if x is clicked change axisLocked to other val
         if(gamepad1.x && xNotPressed){
             xNotPressed = false;
             axisLocked = !axisLocked;
@@ -51,34 +53,17 @@ public class AxisLocking extends OpMode {
             xNotPressed = true;
         }
 
-        //if axis locked is true, auto align then lock y and heading changes
-        if(axisLocked){
-
-            //subtracts right sensor distance from left sensor distance
-            heading = rightDistanceSensor.getDistance(DistanceUnit.CM)
-                                    - leftDistanceSensor.getDistance(DistanceUnit.CM);
-
-            //if diff pos, turn right, if neg, turn left, if zero, its already zero
-            //mult power by alignHeadingFactor
-            if(heading > 0){
-                heading = 1 * alignHeadingFactor;
-            } else if(heading < 0){
-                heading = -1 * alignHeadingFactor;
-            }
-
-
-
-            //stop y and heading change
+        //if axis locked is true, make y axis drive power 0
+        if(axisLocked == true){
             y = 0;
         }
-
 
         // Pass in the rotated input + right stick value for rotation
         drive.setWeightedDrivePower(
                 new Pose2d(
                         x,
                         y,
-                        heading
+                        -gamepad1.right_stick_x
                 )
         );
 
