d90f5a50baa03a6a8ec7acfd6ef8a41c3219b1d3
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-11-20 12:29:57 -0500

    Revert "Updated Box Color Sensor + Controller Detection"
    This reverts commit 7bc88f71c5972b038ed7fdec44e3a853e1e6eb63.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorColorFinder.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorColorFinder.java
@@ -5,21 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-import org.firstinspires.ftc.teamcode.util.Color;
-
 @TeleOp
 public class ColorSensorColorFinder extends OpMode {
     RevColorSensorV3 colorSensor;
     int argbDetected;
-
-    Color yellow = new Color(1500, 2100, 580);
-
-    Color green = new Color(500, 1600, 600);
-
-    Color purple = new Color(1200, 1800, 2500);
-
-    Color white = new Color(3000, 5000, 4500);
-
 
     @Override
     public void init() {
@@ -32,10 +21,6 @@ public class ColorSensorColorFinder extends OpMode {
         telemetry.addData("R", colorSensor.red());
         telemetry.addData("G", colorSensor.green());
         telemetry.addData("B", colorSensor.blue());
-
-        int error = yellow.getError(new Color(colorSensor.red(), colorSensor.green(), colorSensor.blue()));
-
-        telemetry.addData("error", error);
 
         telemetry.update();
     }
