7504024c341da82149f14d4afe554dc346c0f0d3
RobotNatchos <47341210+RobotNatchos@users.noreply.github.com>
2023-11-22 16:14:43 -0500

    Update AutoProgramRedBoard.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgramRedBoard.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgramRedBoard.java
@@ -4,7 +4,6 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -23,7 +22,14 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 @Config
 public class AutoProgramRedBoard extends OpMode {
 
-    //region Dashboard Static Variable Declarations
+    //region Dashboard Variable Declarations
+
+    //region Auto Timer
+
+    public static double SPIKE_OUTTAKE_TIME = 1000; //Time Spike Pixel Outtakes In auto
+    double waitTimer;
+
+    //endregion
 
     //region Slide Variables
     public static int INIT_SLIDE_HEIGHT = 20;
@@ -33,11 +39,9 @@ public class AutoProgramRedBoard extends OpMode {
     //endregion
 
     //region Arm Variables
-    public static double ARM_POSITION = 0.04;
     public static double ARM_SERVO_FORWARD = 0.04;//Stores Value of Arm intake Position
     public static double ARM_SERVO_BACKWARD = 0.7;//Stores Value of Arm outtake Position
 
-    public static double BOX_SERVO_POSITION = 1;
     public static double BOX_SERVO_FORWARD = 1; //Stores Value of Box intake Position
     public static double BOX_SERVO_BACKWARD = 0.3;//Stores Value of Box outtake Position
     //endregion
@@ -53,11 +57,12 @@ public class AutoProgramRedBoard extends OpMode {
     //endregion
 
 
-
-
-    //region Arm Objects
+    //region Intake Objects
     DcMotorEx intakeMotor;
     Servo intakeServo;
+    //endregion
+
+    //region Arm Objects
     CRServo outtakeServo;
     Servo boxServo;
     DcMotorEx leftLinear;
@@ -65,10 +70,6 @@ public class AutoProgramRedBoard extends OpMode {
     Servo armServoLeft;
     Servo armServoRight;
     //endregion
-
-    double waitTimer;
-
-
 
     //region Vision Objects
     TeamPropDetection propDetection = new TeamPropDetection(telemetry);
@@ -78,22 +79,24 @@ public class AutoProgramRedBoard extends OpMode {
     AprilTagHomer aprilTagHomer;
     //endregion
 
-    //region static coordinates
+    //region RR static coordinates
 
     //region red board spike locations
     Pose2d spikeLocation;
-    Pose2d spikeOne = new Pose2d(10,-30, Math.toRadians(180));
-    Pose2d spikeTwo = new Pose2d(20,-24, Math.toRadians(180));
-    Pose2d spikeThree = new Pose2d(13,-30, Math.toRadians(0));
+    public static Pose2d spikeLeft = new Pose2d(10,-30, Math.toRadians(180));
+    public static Pose2d spikeCenter = new Pose2d(20,-24, Math.toRadians(180));
+    public static Pose2d spikeRight = new Pose2d(32,-30, Math.toRadians(180));
     //endregion
 
-    Pose2d redBoardCord = new Pose2d(48, -35, Math.toRadians(180));
-    Pose2d redParkCord = new Pose2d(48, -60, Math.toRadians(180));
+    public static Pose2d STARTING_DRIVE_POS = new Pose2d(10, -62, Math.toRadians(270));
+
+    public static Pose2d redBoardCord = new Pose2d(43, -35, Math.toRadians(180));
+    public static  Pose2d redParkCord = new Pose2d(48, -60, Math.toRadians(180));
+
+    /*
     Pose2d blueBoardCord = new Pose2d(48, 35, Math.toRadians(180));
     Pose2d blueParkCord = new Pose2d(48, 60, Math.toRadians(180));
-
-    Pose2d redBoardCenterSpikeMark = new Pose2d(13, -30, Math.toRadians(0));
-    Pose2d redBoardCenterSpikeMarkStart = new Pose2d(10,62, Math.toRadians(90));
+    */
     //endregion
 
 
@@ -113,6 +116,10 @@ public class AutoProgramRedBoard extends OpMode {
 
     @Override
     public void init() {
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(STARTING_DRIVE_POS);
+
         //region Arm Init
         //region Arm Hardware Map
 
@@ -148,9 +155,9 @@ public class AutoProgramRedBoard extends OpMode {
         //endregion
 
         //region Initial Servo Pos
-        armServoLeft.setPosition(ARM_POSITION);
-        armServoRight.setPosition(1 - ARM_POSITION);
-        boxServo.setPosition(BOX_SERVO_POSITION);
+        armServoLeft.setPosition(ARM_SERVO_FORWARD);
+        armServoRight.setPosition(1 - ARM_SERVO_FORWARD);
+        boxServo.setPosition(BOX_SERVO_FORWARD);
         //endregion
 
         //endregion
@@ -167,45 +174,68 @@ public class AutoProgramRedBoard extends OpMode {
         //endregion
         //endregion
 
+        aprilTagDetector = AprilTagProcessor.easyCreateWithDefaults();
 
-        //get webcam stuff here
+        aprilTagHomer = new AprilTagHomer(aprilTagDetector, drive);
 
-        drive = new SampleMecanumDrive(hardwareMap);
-
-        drive.setPoseEstimate(new Pose2d(10, 62, Math.toRadians(90)));
-
-
+        propDetection = new TeamPropDetection(telemetry);
 
         visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "webcam"), aprilTagDetector, propDetection);
         visionPortal.setProcessorEnabled(aprilTagDetector, false);
-
-        toSpikeMark = drive.trajectoryBuilder(drive.getPoseEstimate())
-            .lineToLinearHeading(spikeLocation)
-                //place purple pixel on spike line
-                .addDisplacementMarker(() -> {
-                    intake.setPower(-1);
-                    outakeServo.setPosition(-.5);
-                })
-                .lineToLinearHeading(redBoardCord)
-                //go to april tag indicated by spike marker location
-                .addDisplacementMarker(() -> {
-                    aprilTagDetector.getDetections();
-                    aprilTagHomer.getCurrentTagPose();
-                    aprilTagHomer.updateDrive();
-                })
-                .lineToLinearHeading(redParkCord)
-                .build();
-
-        drive.followTrajectoryAsync(toSpikeMark);
 
     }
 
     @Override
     public void loop() {
-        switch ()
+        switch (queuedState){
+            case START:
+                if(screenSector != null) {
+                    screenSector = propDetection.getScreenSector();
 
+                    if (screenSector == "L") {
+                        spikeLocation = spikeLeft;
+                    } else if (screenSector == "C") {
+                        spikeLocation = spikeCenter;
+                    } else {
+                        spikeLocation = spikeRight;
+                    }
 
-
+                    queuedState = autoState.TO_SPIKE_MARK;
+                }
+                break;
+            case TO_SPIKE_MARK:
+                if(!drive.isBusy()) {
+                    toSpikeMark = drive.trajectoryBuilder(drive.getPoseEstimate())
+                            .lineToLinearHeading(spikeLocation)
+                            .build();
+                    drive.followTrajectoryAsync(toSpikeMark);
+                    queuedState = autoState.OUTTAKE_SPIKE;
+                }
+                break;
+            case OUTTAKE_SPIKE:
+                if(!drive.isBusy()){
+                    visionPortal.setProcessorEnabled(propDetection, false);
+                    visionPortal.setProcessorEnabled(aprilTagDetector, true);
+                    waitTimer = System.currentTimeMillis() + SPIKE_OUTTAKE_TIME;
+                    intakeMotor.setPower(//reverseIntakePoweHere);
+                    queuedState = autoState.TO_BOARD;
+                }
+                break;
+            case TO_BOARD:
+                if(!drive.isBusy() && System.currentTimeMillis() >= waitTimer){
+                    intakeMotor.setPower(0);
+                    toRedBoard = drive.trajectoryBuilder(toSpikeMark.end())
+                            .lineToLinearHeading(redBoardCord)
+                            .build();
+                    drive.followTrajectoryAsync(toRedBoard);
+                    queuedState = autoState.HOME_TAG;
+                }
+                break;
+            case HOME_TAG:
+                if(!drive.isBusy()){
+                    aprilTagDetector
+                }
+        }
 
         drive.update();
     }
