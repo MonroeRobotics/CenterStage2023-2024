f4899550a0869055a21f482dda3cfbfe281469a7
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-11-27 16:18:38 -0500

    Update suspensionTuner.java
    it rises like a phoenix

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/suspensionTuner.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/suspensionTuner.java
@@ -1,23 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+@TeleOp
 public class suspensionTuner extends OpMode{
 
     DcMotorEx hangMotor;
     @Override
     public void init() {
         hangMotor = hardwareMap.get(DcMotorEx.class, "hangMotor");
+        hangMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
         if(gamepad1.dpad_up) {
-            hangMotor.setPower(1);
-        } else if (gamepad1.dpad_down) {
             hangMotor.setPower(-1);
+        } else if (gamepad1.dpad_down) {
+            hangMotor.setPower(1);
         }
         else {
             hangMotor.setPower(0);
