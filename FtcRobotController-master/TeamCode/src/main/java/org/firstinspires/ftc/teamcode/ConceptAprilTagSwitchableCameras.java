b39cb22a5a702117a9c57fd9cd7a52f76165cb9b
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-12-03 16:09:27 -0500

    Update

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagSwitchableCameras.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagSwitchableCameras.java
@@ -49,7 +49,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: AprilTag Switchable Cameras", group = "Concept")
+@TeleOp(name = "AprilTag Switchable Cameras", group = "Concept")
 @Disabled
 public class ConceptAprilTagSwitchableCameras extends LinearOpMode {
 
