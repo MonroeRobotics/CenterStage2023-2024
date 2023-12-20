e4b46e9490658a9dc4f196120468d8987e0c21ea
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-12-20 16:52:01 -0500

    Update AxonTest.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AxonTest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AxonTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.testing;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
@@ -7,7 +8,16 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
+@Config
 public class AxonTest extends OpMode {
+
+    public double targetPosition = 1;  // Set your desired target position
+    public double kP = 0.1;  // Proportional constant
+    public double kI = 0; // Integral constant
+    public double kD = 0.05; // Derivative constant
+
+    double integral = 0;
+    double previousError = 0;
 
     Servo servo1;
     Servo servo2;
@@ -34,7 +44,7 @@ public class AxonTest extends OpMode {
     @Override
     public void loop() {
 
-        if(currentGamePad.cross && !previousGamePad.cross){
+        /*if(currentGamePad.cross && !previousGamePad.cross){
             servo1.setPosition(0);
             servo2.setPosition(1);
         }
@@ -45,12 +55,41 @@ public class AxonTest extends OpMode {
 
 
         previousGamePad.copy(currentGamePad);
-        currentGamePad.copy(gamepad1);
+        currentGamePad.copy(gamepad1);*/
+        servo1.setPosition(servo1.getPosition() + servoPID());
         telemetry.addData("Servo 1 Reading", servo1Input.getVoltage());
         telemetry.addData("Servo 2 Reading", servo2Input.getVoltage());
 
         telemetry.update();
 
 
+    }
+
+    public double servoPID(){
+        double currentPosition = servo1Input.getVoltage();
+        double error = targetPosition - currentPosition;
+
+        double proportionalTerm = kP * error;
+
+        // Calculate integral term (running sum of errors)
+        // Note: This is a simple integral, more advanced techniques may be needed
+        // such as the trapezoidal rule or anti-windup mechanisms.
+        double integralTerm = kI * (integral + error);
+
+        // Calculate derivative term (change in error)
+        double derivativeTerm = kD * (error - previousError);
+
+        // Calculate the output power
+        double output = proportionalTerm + integralTerm + derivativeTerm;
+
+        // Update previous error and integral for the next iteration
+        previousError = error;
+        integral += error;
+
+        telemetry.addData("Target Position", targetPosition);
+        telemetry.addData("Current Position", currentPosition);
+        telemetry.addData("Output", output);
+
+        return output;
     }
 }
