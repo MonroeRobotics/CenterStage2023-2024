f4943d361293ec8719dd132916bc743f2b21139f
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2023-12-20 11:48:46 -0500

    Revert "Revert "Autonomous Test 2 (#18)""
    This reverts commit 4cd3aea3b3ec5270073960ac7977b71b6d38dd14.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ChangeableProcessor.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ChangeableProcessor.java
@@ -1,8 +1,6 @@
-package org.firstinspires.ftc.teamcode.testing;
+package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -11,21 +9,17 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @Autonomous
-@Config
-@Disabled
 public class ChangeableProcessor extends OpMode {
     TeamPropDetection propDetector;
     AprilTagProcessor aprilTagDetector;
     VisionPortal visionPortal;
     String screenSector;
 
-    public static String alliance = "red";
-
     boolean cameraState = false;
     boolean notPressed = true;
     @Override
     public void init() {
-        propDetector = new TeamPropDetection(alliance);
+        propDetector = new TeamPropDetection("red");
         aprilTagDetector = AprilTagProcessor.easyCreateWithDefaults();
         visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "webcam"), aprilTagDetector, propDetector);
         visionPortal.setProcessorEnabled(aprilTagDetector, false);
