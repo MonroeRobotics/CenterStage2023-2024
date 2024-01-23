0354362b07e4ddb08acbd4ea1cca96287baa8a88
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2024-01-23 15:29:42 -0500

    Update TrackingHelperTest.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TrackingHelperTest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TrackingHelperTest.java
@@ -8,16 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.util.TrackingHelper;
-
-/**
- * This is a simple teleop routine for testing localization. Drive the robot around like a normal
- * teleop routine and make sure the robot's estimated pose matches the robot's actual pose (slight
- * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
- * exercise is to ascertain whether the localizer has been configured properly (note: the pure
- * encoder localizer heading may be significantly off if the track width has not been tuned).
- */
 @TeleOp(group = "drive")
-@Disabled
 public class TrackingHelperTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
