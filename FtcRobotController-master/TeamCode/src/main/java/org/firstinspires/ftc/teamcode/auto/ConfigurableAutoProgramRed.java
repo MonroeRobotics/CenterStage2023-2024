8d6fa822570a120672fceafacdf4ceabbb7820a0
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2024-01-26 16:55:18 -0500

    Update ConfigurableAutoProgramRed.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ConfigurableAutoProgramRed.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ConfigurableAutoProgramRed.java
@@ -60,6 +60,8 @@ public class ConfigurableAutoProgramRed extends LinearOpMode {
     Trajectory toRedBoard;
     Trajectory redBoardPark1;
     Trajectory redBoardPark2;
+    Trajectory toPreTruss;
+    Trajectory toPostTruss;
     //endregion
 
     ArmController armController;
@@ -83,10 +85,13 @@ public class ConfigurableAutoProgramRed extends LinearOpMode {
     //region red board spike locations
     Pose2d spikeLocation;
 
-    Pose2d spikeLeft = new Pose2d(4,-40, Math.toRadians(315));
-    Pose2d spikeCenter = new Pose2d(12,-34.5, Math.toRadians(270));
-    Pose2d spikeRight = new Pose2d(19,-37, Math.toRadians(240));
+    Pose2d spikeLeft;
+    Pose2d spikeCenter;
+    Pose2d spikeRight;
     //endregion
+
+    public static Pose2d beforeTrussCord = new Pose2d(-36, -12, Math.toRadians(180));
+    public static Pose2d afterTrussCord = new Pose2d(30, -12, Math.toRadians(180));
 
     public static Pose2d STARTING_DRIVE_POS = new Pose2d(10, -62, Math.toRadians(270));
 
@@ -101,6 +106,8 @@ public class ConfigurableAutoProgramRed extends LinearOpMode {
         START,
         TO_SPIKE_MARK,
         VISION_SWITCH,
+        PRE_TRUSS,
+        POST_TRUSS,
         TO_BOARD,
         HOME_TAG,
         PLACE_BOARD,
@@ -175,6 +182,16 @@ public class ConfigurableAutoProgramRed extends LinearOpMode {
 
             switch (queuedState) {
                 case START:
+                    if(autoConfiguration.getStartPosition() == AutoConfiguration.StartPosition.BOARD){
+                        spikeLeft = new Pose2d(4,-40, Math.toRadians(315));
+                        spikeCenter = new Pose2d(12,-34.5, Math.toRadians(270));
+                        spikeRight = new Pose2d(19,-37, Math.toRadians(240));
+                    }else{
+                        spikeRight = new Pose2d(-33,-28, Math.toRadians(180));
+                        spikeCenter = new Pose2d(-35,-33, Math.toRadians(270));
+                        spikeLeft = new Pose2d(-42,-35, Math.toRadians(315));
+                    }
+
                     screenSector = propDetection.getScreenSector();
                     if (screenSector != null) {
                         if (screenSector.equals("L")) {
@@ -236,6 +253,36 @@ public class ConfigurableAutoProgramRed extends LinearOpMode {
                     GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
                     gainControl.setGain(CAMERA_GAIN);*/
                         queuedState = autoState.TO_BOARD;
+                    }
+                    break;
+                case PRE_TRUSS:
+                    if(!drive.isBusy()){
+//                    drive.setPoseEstimate(new Pose2d(drive.getPoseEstimate().getX(), drive.getPoseEstimate().getY(), (drive.getExternalHeading() + TURN_ADJ)));
+                        visionPortal.setProcessorEnabled(propDetection, false);
+                        visionPortal.setProcessorEnabled(aprilTagDetector, true);
+                    /*ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+                    if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                        exposureControl.setMode(ExposureControl.Mode.Manual);
+                    }
+                    exposureControl.setExposure((long)CAMERA_EXPOSURE, TimeUnit.MILLISECONDS);
+
+                    // Set Gain.
+                    GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+                    gainControl.setGain(CAMERA_GAIN);*/
+                        toPreTruss = drive.trajectoryBuilder(drive.getPoseEstimate())
+                                .lineToLinearHeading(beforeTrussCord)
+                                .build();
+                        drive.followTrajectoryAsync(toPreTruss);
+                        queuedState = AutoProgramRedAway.autoState.POST_TRUSS;
+                    }
+                    break;
+                case POST_TRUSS:
+                    if(!drive.isBusy()){
+                        toPostTruss = drive.trajectoryBuilder(drive.getPoseEstimate())
+                                .lineToLinearHeading(afterTrussCord)
+                                .build();
+                        drive.followTrajectoryAsync(toPostTruss);
+                        queuedState = AutoProgramRedAway.autoState.TO_BOARD;
                     }
                     break;
                 case TO_BOARD:
