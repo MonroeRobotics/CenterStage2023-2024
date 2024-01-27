781fa89b4cdabfb8d262ecc30e6fbd9f0de31945
RobotNatchos <47341210+RobotNatchos@users.noreply.github.com>
2024-01-27 12:06:57 -0500

    Disable Op Modes for cleaning

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AutoConfigTester.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AutoConfigTester.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.testing;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.util.AutoConfiguration;
 
 @TeleOp
+@Disabled
 public class AutoConfigTester extends LinearOpMode {
     Gamepad currentGamepad;
     Gamepad previousGamepad;
